bd6fcb8da294b7defc7d273de8abfc3e96bd5a46
Zeljko Vukovic <zeljkov@uns.ac.rs>
2019-11-16 18:24:23 +0100

    lab4-end

---
+++ lab4-end/src/main/java/jwd/wafepa/model/Activity.java
@@ -0,0 +1,83 @@
+package jwd.wafepa.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Models a physical activity.
+ * 
+ *
+ */
+@Entity
+@Table(name="tbl_activity")
+public class Activity {
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	@Column(length = 50, nullable = false)
+	private String name;
+	@Column(name="adm_comment")
+	private String adminComment="Test";
+	
+
+	public Activity() {
+		super();
+	}
+
+	public Activity(String name) {
+		super();
+		this.name = name;
+	}
+
+	public Activity(Long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	
+	/**
+	 * 
+	 * @return Activity identifier.
+	 */
+	public Long getId() {
+		return id;
+	}
+	
+	/**
+	 * Sets activity identifier.
+	 * @param id new identifier
+ 	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	/**
+	 * 
+	 * @return Name of the activity.
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * 
+	 * @param name New name of the activity.
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAdminComment() {
+		return adminComment;
+	}
+
+	public void setAdminComment(String adminComment) {
+		this.adminComment = adminComment;
+	}
+	
+	
+}
