bf5eedbd22ae5cd82877e85339a000071c8cafb0
nenadtodEdu <nenadtod@uns.ac.rs>
2019-11-25 17:38:47 +0100

    lab8-begin

---
+++ lab8-begin/src/main/resources/static/app/js/main.js
@@ -0,0 +1,249 @@
+var wafepaApp = angular.module("wafepaApp",["ngRoute"]);
+
+wafepaApp.controller("HomeCtrl", function($scope){
+	$scope.message="Hello JWD40";
+});
+
+wafepaApp.controller("ActivitiesCtrl", function($scope, $http, $location){
+	
+	var url = "/api/activities";
+	
+	$scope.activities = [];
+	
+	var getActivities = function(){
+		var promise = $http.get(url);
+		promise.then(
+			function success(res){
+				$scope.activities = res.data;
+				//console.log("test1");
+				//console.log(res);
+			},
+			function error(res){
+				alert("Couldn't fetch activities.");
+			}
+		);
+		//console.log("test2");
+	}
+	
+	getActivities();
+	
+	$scope.goToEdit = function(id){
+		$location.path("/activities/edit/" + id);
+	}
+	
+	$scope.goToAdd = function(){
+		$location.path("/activities/add");
+	}
+	
+});
+
+
+wafepaApp.controller("EditActivityCtrl", function($scope,$http, $routeParams, $location){
+	//console.log($routeParams);
+	
+	var url = "/api/activities/" + $routeParams.aid;
+	
+	$scope.activity = {};
+	$scope.activity.name = "";
+	
+	var getActivity = function(){
+		var promise = $http.get(url);
+		promise.then(
+			function uspeh(odg){
+				$scope.activity = odg.data;
+			},
+			function neuspeh(odg){
+				alert("Couldn't fetch the activity.");
+			}
+		);
+	}
+	
+	getActivity();
+	
+	$scope.doEdit = function(){
+		var promise = $http.put(url, $scope.activity);
+		promise.then(
+			function success(res){
+				$location.path("/activities");
+			},
+			function error(){
+				alert("Couldn't save the activity");
+			}
+		);
+	}
+	
+});
+
+
+wafepaApp.controller("AddActivityCtrl", function($scope, $http, $location){
+	
+	$scope.activity = {};
+	$scope.activity.name = "";
+	
+	var url = "/api/activities";
+	
+	$scope.doAdd = function(){
+		$http.post(url, $scope.activity).then(
+			function success(){
+				$location.path("/activities");
+			},
+			function error(){
+				alert("Couldn't save the activity.");
+			}
+		);
+	}
+	
+});
+
+
+
+
+wafepaApp.controller("RecordsCtrl", function($scope, $http, $location){
+	
+	var url = "/api/records";
+	
+	$scope.records = [];
+	
+	$scope.newRecord = {};
+	$scope.newRecord.time = "";
+	$scope.newRecord.duration = "";
+	$scope.newRecord.intensity = "";
+	//TODO: dodati obeležja kojim se povezuje sa korisnikom i aktivnošću
+	
+	
+	var getRecords = function(){
+		var promise = $http.get(url);
+		promise.then(
+			function success(res){
+				$scope.records = res.data;
+			},
+			function error(){
+				alert("Couldn't fetch records");
+			}
+		);
+	}
+	
+	getRecords();
+	
+	//TODO: Obezbediti prihvat korisnika i aktivnosti
+	
+	
+	
+	
+	$scope.doAdd = function(){
+		
+		$http.post(url, $scope.newRecord).then(
+			function success(res){
+				getRecords();
+	
+			},
+			function error(){
+				alert("Couldn't save the record");
+			}
+		);
+	}
+	
+	$scope.goToEdit = function(id){
+		$location.path("/records/edit/" + id);
+	}
+});
+
+
+wafepaApp.controller("EditRecordCtrl", function($scope, $http, $routeParams, $location){
+	
+	var recordUrl = "/api/records/" + $routeParams.id;
+	var activitiesUrl = "/api/activities";
+	var usersUrl = "/api/users";
+	
+	$scope.record = {};
+	$scope.record.time = "";
+	$scope.record.duration = "";
+	$scope.record.intensity = "";
+	$scope.record.userId = "";
+	$scope.record.activityId = "";	
+	
+	
+	$scope.activities = [];
+	$scope.users = [];
+	
+	
+	
+	var getActivities = function(){
+		$http.get(activitiesUrl).then(
+			function success(res){
+				$scope.activities = res.data;
+			},
+			function error(){
+				alert("Couldn't fetch activities");
+			}
+		);
+	}
+	
+	var getUsers = function(){
+		return $http.get(usersUrl).then(
+			function success(res){
+				$scope.users = res.data;
+			},
+			function error(){
+				alert("Couldn't fetch users.");
+			}
+		);
+	}
+	
+	var getRecord = function(){
+		$http.get(recordUrl).then(
+			function success(res){
+				$scope.record = res.data;
+			},
+			function error(){
+				alert("Couldn't fetch record.");
+			}
+		);
+	}
+	
+	//TODO: Obezbediti redosled izvrsavanja!
+	getActivities();
+	getUsers();
+	getRecord();
+	
+	
+	$scope.doEdit = function(){
+		$http.put(recordUrl, $scope.record).then(
+			function success(){
+				$location.path("/records");
+			},
+			function error(){
+				alert("Something went wrong.");
+			}
+		);
+	}
+});
+
+
+
+
+wafepaApp.config(['$routeProvider', function($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl : '/app/html/home.html',
+			controller: "HomeCtrl"
+		})
+		.when('/activities', {
+			templateUrl : '/app/html/activities.html'
+		})
+		.when('/activities/add', {
+			templateUrl : '/app/html/add-activity.html'
+		})
+		.when('/activities/edit/:aid', {
+			templateUrl : '/app/html/edit-activity.html'
+		})
+		.when('/records', {
+			templateUrl : '/app/html/records.html'
+		})
+		.when('/records/edit/:id', {
+			templateUrl : '/app/html/edit-record.html'
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+}]);
